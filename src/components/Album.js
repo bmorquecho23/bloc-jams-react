49c0045a6a1040831c6132351bdd45f92404349b
Billy Morquecho <billymorquecho@gmail.com>
2018-02-19 11:38:24 -0600

    added Playerbar

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
 class Album extends Component {
   constructor(props){
@@ -44,6 +45,22 @@ handleSongClick(song){
     this.play();
   }
 }
+handlePrevClick(){
+  const currentIndex=this.state.album.songs.findIndex(song=>this.state.currentSong === song);
+  const newIndex = Math.max(0, currentIndex - 1);
+  const newSong = this.state.album.songs[newIndex];
+  this.setSong(newSong);
+  this.play(newSong);
+}
+handleNextClick(){
+  const currentIndex=this.state.album.songs.findIndex(song=>this.state.currentSong === song);
+  const newIndex = currentIndex + 1;
+  if(newIndex >= this.state.album.songs.length) {return};
+  const newSong = this.state.album.songs[newIndex];
+  this.setSong(newSong);
+  this.play(newSong);
+}
+
 
   render() {
     return (
@@ -72,6 +89,13 @@ handleSongClick(song){
              )}
           </tbody>
         </table>
+        <PlayerBar
+          isPlaying={this.state.isPlaying}
+          currentSong={this.state.currentSong}
+          handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+          handlePrevClick={()=> this.handlePrevClick()}
+          handleNextClick={()=> this.handleNextClick()}
+        />
       </section>
     );
   }
